aa6700a8398194704ec62a75a8bcc7d633d5b4e7
Nandart <reguinenu@gmail.com>
2025-05-04 19:50:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
-import basicPlugin from '@vitejs/plugin-basic'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/nandart-3d/',
-  plugins: [basicPlugin()],
 })
-

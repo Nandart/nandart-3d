1ccc1f2406c0669936835b3b933c3031c2080e2c
Nandart <reguinenu@gmail.com>
2025-05-05 00:52:09 +0100

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-import * as THREE from './three/build/three.module.js'
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js'
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js'
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/controls/OrbitControls.js'
 
 // Cena e câmara
 const scene = new THREE.Scene()

355d25846431c4ce85cc447eb31385e9ad709294
Nandart <reguinenu@gmail.com>
2025-05-04 23:52:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-export default {
-  root: '.',
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', 
   build: {
-    outDir: 'dist'
-  }
-}
+    outDir: 'dist', 
+    target: 'esnext', 
+    sourcemap: true, 
+  },
+  resolve: {
+    alias: {
+      '@': '/src', '
+    },
+  },
+  server: {
+    port: 3000, 
+    open: true, 
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+});

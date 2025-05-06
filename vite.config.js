8027464eef82470784ce17a47b0aca5431719d23
Nandart <reguinenu@gmail.com>
2025-05-06 23:12:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-  root: resolve(__dirname, './'),  // Define a raiz explicitamente
-  plugins: [
-    nodeResolve({
-      browser: true,
-      dedupe: ['three'],
-      extensions: ['.js', '.json']
-    })
-  ],
+  root: resolve(__dirname, './'),
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html')
-      },
-      preserveEntrySignatures: 'strict'
+      }
     }
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './'),  // Alias absoluto
+      '@': resolve(__dirname, './src'),
       'three': resolve(__dirname, 'node_modules/three/build/three.module.js')
-    },
-    dedupe: ['three']  // Evita duplicação
+    }
   },
-  base: './'  // Base relativa para todos os assets
+  base: './'
 });

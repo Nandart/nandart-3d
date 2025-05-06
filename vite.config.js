2d1e8c744d80b7d8961c92c2fb0f1c6bb42862da
Nandart <reguinenu@gmail.com>
2025-05-06 20:50:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, './'),
-  base: '/',
-  publicDir: 'public',
-  
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
-  },
-  
-  server: {
-    port: 3000,
-    fs: {
-      strict: false
-    }
+    assetsInlineLimit: 0
   }
 });

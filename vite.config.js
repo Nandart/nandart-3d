2d9d47501f7d01fcd4145ff672cc70d25bc03ac1
Nandart <reguinenu@gmail.com>
2025-05-06 23:26:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
+  plugins: [
+    nodeResolve({
+      browser: true,
+      dedupe: ['three']
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   resolve: {
     alias: {
+      'three': resolve(__dirname, 'node_modules/three/build/three.module.js'),
       'three/examples/jsm/': resolve(__dirname, 'node_modules/three/examples/jsm/')
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['three/examples/jsm/loaders/FontLoader.js']
-    }
-  }
+  base: './'
 });
-

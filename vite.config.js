8921dd6f717fc8a1a5ff91b280ae4e6fd343dd24
Nandart <reguinenu@gmail.com>
2025-05-08 19:34:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', // A raiz é o próprio diretório do projeto
-  base: './', // Importante para GitHub Pages e Vercel
+  root: '.',
+  base: '/',
   build: {
-    outDir: 'dist',
-    target: 'esnext',
-    sourcemap: true,
-    rollupOptions: {
-      input: './index.html'
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
+    outDir: 'dist'
   }
 });

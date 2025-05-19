f154b326c38149b0057d01338f26ffcfce997375
Nandart <reguinenu@gmail.com>
2025-05-19 22:48:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
-  resolve: {
-    alias: {
-      'three': resolve(__dirname, 'node_modules/three/build/three.module.js'),
-      'three/addons/': resolve(__dirname, 'node_modules/three/examples/jsm/')
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['three', 'three/addons/*', 'gsap', 'ethers'],
-      output: {
-        manualChunks: {
-          three: ['three'],
-          gsap: ['gsap'],
-          ethers: ['ethers']
-        }
-      }
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
     }
   },
   optimizeDeps: {
@@ -26,10 +12,7 @@ export default defineConfig({
       'three',
       'three/examples/jsm/objects/Reflector',
       'three/examples/jsm/loaders/FontLoader',
-      'three/examples/jsm/geometries/TextGeometry',
-      'gsap',
-      'ethers'
-    ],
-    exclude: ['three/examples/jsm/controls/OrbitControls']
+      'three/examples/jsm/geometries/TextGeometry'
+    ]
   }
-})
+});

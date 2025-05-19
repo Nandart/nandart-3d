4e088142b8d2b81700c3fb62e664d06d57e127e9
Nandart <reguinenu@gmail.com>
2025-05-19 22:18:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,35 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      'three': resolve(__dirname, 'node_modules/three/build/three.module.js'),
+      'three/addons/': resolve(__dirname, 'node_modules/three/examples/jsm/')
     }
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        info: resolve(__dirname, 'info.html')
+      external: ['three', 'three/addons/*', 'gsap', 'ethers'],
+      output: {
+        manualChunks: {
+          three: ['three'],
+          gsap: ['gsap'],
+          ethers: ['ethers']
+        }
       }
     }
   },
-  server: {
-    open: true
+  optimizeDeps: {
+    include: [
+      'three',
+      'three/examples/jsm/objects/Reflector',
+      'three/examples/jsm/loaders/FontLoader',
+      'three/examples/jsm/geometries/TextGeometry',
+      'gsap',
+      'ethers'
+    ],
+    exclude: ['three/examples/jsm/controls/OrbitControls']
   }
-});
+})

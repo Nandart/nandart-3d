70a0a4b812680cc20cdfc4ef2ee67e58bfe93344
Nandart <reguinenu@gmail.com>
2025-05-21 01:06:23 +0100

    Create nftContract.js

---
+++ nftContract.js
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+
+// Endereço do contrato deployado na blockchain (copiado do Remix)
+const CONTRACT_ADDRESS = "0xD91...391"; // substitui pelo teu
+
+// ABI do contrato (podes copiar da aba "ABI" no Remix)
+import CONTRACT_ABI from "./NandartNFT_ABI.json";
+
+export async function connectWallet() {
+  if (!window.ethereum) throw new Error("MetaMask não detectada");
+
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const accounts = await provider.send("eth_requestAccounts", []);
+  const signer = await provider.getSigner();
+
+  return { signer, account: accounts[0], contract: new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer) };
+}

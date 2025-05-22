f11369534ae1bbd4c9a6002c6acd32bc2eaa6b5f
Nandart <reguinenu@gmail.com>
2025-05-22 12:36:02 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -590,7 +590,6 @@ function criarObrasNormais() {
 // ==================== BLOCO 10 — ANIMAÇÃO CONTÍNUA DAS OBRAS CIRCULARES ====================
 
 let anguloAtual = 0;
-let obraDestacada = null;
 let ambienteDesacelerado = false;
 
 function animarObrasCirculares(delta) {

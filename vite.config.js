b5cf49fe6d30616b465790daf1ec7d67171cb1bd
Nandart <reguinenu@gmail.com>
2025-05-24 17:44:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,18 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    nodeResolve({
-      browser: true,
-      preferBuiltins: false
-    })
-  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        manualChunks: {
-          three: ['three'],
-          gsap: ['gsap'],
-          ethers: ['ethers']
-        }
       }
     }
   },
   server: {
-    https: true,
     open: true
   }
 });

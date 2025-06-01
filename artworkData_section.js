fe1d3f70ae3492e2507691a03c2daa0e51da0f8a
Nandart <reguinenu@gmail.com>
2025-06-01 21:18:16 +0100

    Add files via upload

---
+++ artworkData_section.js
@@ -0,0 +1,66 @@
+const artworkData = [
+  {
+    "title": "Fragment of Eternity",
+    "artist": "In\u00eas Duarte",
+    "year": "2023",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeicnafmqkac6fxctcnuibyr5bv35srkk24iled3p72n4oqul333vaa/fragment_of_eternity.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Shadows of Light",
+    "artist": "Miguel Costa",
+    "year": "2024",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeic2cmwwsvm6fjfbxojc2pcr3rzvtfveludgpd44jxgetyrlncxmyu/shadows_of_light.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Shared Horizon",
+    "artist": "Clara Mendon\u00e7a",
+    "year": "2022",
+    "price": "1",
+    "tokenURI": "ipfs://bafybeiakxmorl2p2z7cjm7vl5laqc5iozulevcskrya7hrmm6biewzwzu4/shared_horizon.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Memories of Silence",
+    "artist": "Rui Valente",
+    "year": "2023",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeibdrkwcyu6qajyynrl2sjgwyiowpgwwu5limjq6rab4wdoggnflpy/memories_of_silence.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Contained Rhythm",
+    "artist": "Joana Serra",
+    "year": "2025",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeidi67ljb4oa7ywti4dxcnlmvnjfme6gyongbl3ygsgmpg5ne5tqz4/contained_rhythm.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Inner Fluctuation",
+    "artist": "Andr\u00e9 Luz",
+    "year": "2023",
+    "price": "1.0",
+    "tokenURI": "ipfs://bafybeihnvhjtbkygyw7tcghpxbbzcte2a74mqkswaliu3gec4r6tm7phvm/inner_fluctuation.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Concealed Verse",
+    "artist": "Sofia Rocha",
+    "year": "2024",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeify2nqjijzo3ocy7um5pot75bzmhqgtkmnxa5izf5mgfxy6jvhmjm/concealed_verse.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    "title": "Silhouette of Tomorrow",
+    "artist": "Tiago Faria",
+    "year": "2025",
+    "price": "0.0001",
+    "tokenURI": "ipfs://bafybeicslxf77trep4rwsszl57jhwmlmevuc4bnfouvqdrjpc4jgggfv44/silhouette_of_tomorrow.json",
+    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  }
+];

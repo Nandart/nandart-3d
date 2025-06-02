4a3023b84bc3c1e9d029a95d88a31f13f16d40cc
Nandart <reguinenu@gmail.com>
2025-06-02 21:30:17 +0100

    Delete contrato.js

--- contrato.js
+++
@@ -1,7 +0,0 @@
-async function cunharObras() {
-  const contrato = await getContrato();
-  const contas = await window.ethereum.request({ method: "eth_requestAccounts" });
-  const user = contas[0];
-
-  await mintTodos(contrato, user);
-}

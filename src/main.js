ee27183336a738ddc7ee27f74c45813b61e85fc9
Nandart <reguinenu@gmail.com>
2025-06-08 21:53:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { getContrato } from "./contrato.js";
-import './js/cunharSuspensas.js';
+import './api/cunharSuspensas.js';
 import { comprarObra, revenderObra, linkOpenSea } from "./market.js";
 // Versão final com iluminação específica para paredes
 import * as THREE from 'three';

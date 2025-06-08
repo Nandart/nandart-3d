d4f0264d298fde5ae925a802696bdd70e78217ae
Nandart <reguinenu@gmail.com>
2025-06-08 16:38:54 +0100

    Update artworkData_section.js

--- artworkData_section.js
+++ artworkData_section.js
@@ -4,7 +4,7 @@ const artworkData = [
     "artist": "In\u00eas Duarte",
     "year": "2023",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeicnafmqkac6fxctcnuibyr5bv35srkk24iled3p72n4oqul333vaa/fragment_of_eternity.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeicnafmqkac6fxctcnuibyr5bv35srkk24iled3p72n4oqul333vaa/fragment_of_eternity.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -12,7 +12,7 @@ const artworkData = [
     "artist": "Miguel Costa",
     "year": "2024",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeic2cmwwsvm6fjfbxojc2pcr3rzvtfveludgpd44jxgetyrlncxmyu/shadows_of_light.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeic2cmwwsvm6fjfbxojc2pcr3rzvtfveludgpd44jxgetyrlncxmyu/shadows_of_light.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -20,7 +20,7 @@ const artworkData = [
     "artist": "Clara Mendon\u00e7a",
     "year": "2022",
     "price": "1",
-    "tokenURI": "ipfs://bafybeiakxmorl2p2z7cjm7vl5laqc5iozulevcskrya7hrmm6biewzwzu4/shared_horizon.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeiakxmorl2p2z7cjm7vl5laqc5iozulevcskrya7hrmm6biewzwzu4/shared_horizon.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -28,7 +28,7 @@ const artworkData = [
     "artist": "Rui Valente",
     "year": "2023",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeibdrkwcyu6qajyynrl2sjgwyiowpgwwu5limjq6rab4wdoggnflpy/memories_of_silence.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeibdrkwcyu6qajyynrl2sjgwyiowpgwwu5limjq6rab4wdoggnflpy/memories_of_silence.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -36,7 +36,7 @@ const artworkData = [
     "artist": "Joana Serra",
     "year": "2025",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeidi67ljb4oa7ywti4dxcnlmvnjfme6gyongbl3ygsgmpg5ne5tqz4/contained_rhythm.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeidi67ljb4oa7ywti4dxcnlmvnjfme6gyongbl3ygsgmpg5ne5tqz4/contained_rhythm.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -44,7 +44,7 @@ const artworkData = [
     "artist": "Andr\u00e9 Luz",
     "year": "2023",
     "price": "1.0",
-    "tokenURI": "ipfs://bafybeihnvhjtbkygyw7tcghpxbbzcte2a74mqkswaliu3gec4r6tm7phvm/inner_fluctuation.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeihnvhjtbkygyw7tcghpxbbzcte2a74mqkswaliu3gec4r6tm7phvm/inner_fluctuation.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -52,7 +52,7 @@ const artworkData = [
     "artist": "Sofia Rocha",
     "year": "2024",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeify2nqjijzo3ocy7um5pot75bzmhqgtkmnxa5izf5mgfxy6jvhmjm/concealed_verse.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeify2nqjijzo3ocy7um5pot75bzmhqgtkmnxa5izf5mgfxy6jvhmjm/concealed_verse.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
@@ -60,7 +60,7 @@ const artworkData = [
     "artist": "Tiago Faria",
     "year": "2025",
     "price": "0.0001",
-    "tokenURI": "ipfs://bafybeicslxf77trep4rwsszl57jhwmlmevuc4bnfouvqdrjpc4jgggfv44/silhouette_of_tomorrow.json",
+    "tokenURI": "https://ipfs.io/ipfs/bafybeicslxf77trep4rwsszl57jhwmlmevuc4bnfouvqdrjpc4jgggfv44/silhouette_of_tomorrow.json",
     "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   }
 ];

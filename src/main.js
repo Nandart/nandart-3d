13ffe54b5add77e2bf24bebfd0832f73450b8ad0
Nandart <reguinenu@gmail.com>
2025-06-10 21:35:50 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -649,7 +649,63 @@ const artworkData = [
     artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   }
 ];
-export const obrasSuspensas = []; // ou adicione os elementos necessários
+export const obrasSuspensas = [  {
+    title: "Shadows of Light",
+    artist: "Rénner Nunes",
+    year: "2024",
+    price: "0.01",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/shadows_of_light.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    title: "Shared Horizon",
+    artist: "Rénner Nunes",
+    year: "2022",
+    price: "1",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/shared_horizon.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    title: "Memories of Silence",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "0.15",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/memories_of_silence.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    title: "Contained Rhythm",
+    artist: "Rénner Nunes",
+    year: "2025",
+    price: "0.0001",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/contained_rhythm.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    title: "Inner Fluctuation",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "0.2",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/inner_fluctuation.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  },
+  {
+    title: "Fragments of Identity",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "1.55",
+    tokenURI: "ipfs://bafkreieevpfuos62jiiflarwwpgubw3znzsfv5fjl3k2tv2uiykpeiueee",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41",
+    TokenId: 15
+  },
+  {
+    title: "Silhouette of Tomorrow",
+    artist: "Rénner Nunes",
+    year: "2024",
+    price: "0.04",
+    tokenURI: "ipfs://bafkreif45vd7woswi3pbdzrowr6mtkobu6e7wop4yj3lukn4l4lm6k6h6y",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+  }]; // ou adicione os elementos necessários
 
 const artworks = [];
 const artworkReflections = [];

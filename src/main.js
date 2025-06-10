97f0e8c0c55cf65a27ec91164a163086dbf378c0
Nandart <reguinenu@gmail.com>
2025-06-10 23:27:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -653,9 +653,9 @@ const artworkData = [
     artist: "Rénner Nunes",
     year: "2023",
     price: "1.55",
-    tokenURI: "ipfs://bafkreieevpfuos62jiiflarwwpgubw3znzsfv5fjl3k2tv2uiykpeiueee/15",
+    tokenURI: "ipfs://bafkreieevpfuos62jiiflarwwpgubw3znzsfv5fjl3k2tv2uiykpeiueee",
     artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41",
-    TokenId: 15
+    tokenId: 15
   },
   {
     title: "Silhouette of Tomorrow",

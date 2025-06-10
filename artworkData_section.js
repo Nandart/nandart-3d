f5800eb7478b2fb065e5104a327a0a24d0f012d8
Nandart <reguinenu@gmail.com>
2025-06-10 23:21:11 +0100

    Update artworkData_section.js

--- artworkData_section.js
+++ artworkData_section.js
@@ -1,66 +1,66 @@
 const artworkData = [
+ {
+  title: "Fragment of Eternity",
+  artist: "Rénner Nunes",
+  year: "2023",
+  price: "0.08",
+  tokenURI: "https://ipfs.io/ipfs/bafkreibhrxsmbi6t36qupa5zw6mrc5n5voirsclvkkolobj7wudm5emot4/fragment_of_eternity.json",
+  artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+},
   {
-    "title": "Fragment of Eternity",
-    "artist": "In\u00eas Duarte",
-    "year": "2023",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeicnafmqkac6fxctcnuibyr5bv35srkk24iled3p72n4oqul333vaa/fragment_of_eternity.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Shadows of Light",
+    artist: "Rénner Nunes",
+    year: "2024",
+    price: "0.01",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/shadows_of_light.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
-    "title": "Shadows of Light",
-    "artist": "Miguel Costa",
-    "year": "2024",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeic2cmwwsvm6fjfbxojc2pcr3rzvtfveludgpd44jxgetyrlncxmyu/shadows_of_light.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Shared Horizon",
+    artist: "Rénner Nunes",
+    year: "2022",
+    price: "1",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/shared_horizon.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
-    "title": "Shared Horizon",
-    "artist": "Clara Mendon\u00e7a",
-    "year": "2022",
-    "price": "1",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeiakxmorl2p2z7cjm7vl5laqc5iozulevcskrya7hrmm6biewzwzu4/shared_horizon.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Memories of Silence",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "0.15",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/memories_of_silence.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
-    "title": "Memories of Silence",
-    "artist": "Rui Valente",
-    "year": "2023",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeibdrkwcyu6qajyynrl2sjgwyiowpgwwu5limjq6rab4wdoggnflpy/memories_of_silence.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Contained Rhythm",
+    artist: "Rénner Nunes",
+    year: "2025",
+    price: "0.0001",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/contained_rhythm.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
-    "title": "Contained Rhythm",
-    "artist": "Joana Serra",
-    "year": "2025",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeidi67ljb4oa7ywti4dxcnlmvnjfme6gyongbl3ygsgmpg5ne5tqz4/contained_rhythm.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Inner Fluctuation",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "0.2",
+    tokenURI: "https://ipfs.io/ipfs/bafybeia6bbrqltffiwc4hq6zfwciybwdjmwi3z7hn4w3wo7b3mbaxshfqy/inner_fluctuation.json",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   },
   {
-    "title": "Inner Fluctuation",
-    "artist": "Andr\u00e9 Luz",
-    "year": "2023",
-    "price": "1.0",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeihnvhjtbkygyw7tcghpxbbzcte2a74mqkswaliu3gec4r6tm7phvm/inner_fluctuation.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Fragments of Identity",
+    artist: "Rénner Nunes",
+    year: "2023",
+    price: "1.55",
+    tokenURI: "ipfs://bafkreieevpfuos62jiiflarwwpgubw3znzsfv5fjl3k2tv2uiykpeiueee",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41",
+    TokenId: 15
   },
   {
-    "title": "Concealed Verse",
-    "artist": "Sofia Rocha",
-    "year": "2024",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeify2nqjijzo3ocy7um5pot75bzmhqgtkmnxa5izf5mgfxy6jvhmjm/concealed_verse.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
-  },
-  {
-    "title": "Silhouette of Tomorrow",
-    "artist": "Tiago Faria",
-    "year": "2025",
-    "price": "0.0001",
-    "tokenURI": "https://ipfs.io/ipfs/bafybeicslxf77trep4rwsszl57jhwmlmevuc4bnfouvqdrjpc4jgggfv44/silhouette_of_tomorrow.json",
-    "artista": "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
+    title: "Silhouette of Tomorrow",
+    artist: "Rénner Nunes",
+    year: "2024",
+    price: "0.04",
+    tokenURI: "ipfs://bafkreif45vd7woswi3pbdzrowr6mtkobu6e7wop4yj3lukn4l4lm6k6h6y",
+    artista: "0x913b3984583Ac44dE06Ef480a8Ac925DEA378b41"
   }
-];
